321c71b987f8c89fbac7878fb0f15f3289ec23d1
Sjors Kaagman <sjorspnk@gmail.com>
2018-06-02 23:22:09 +0200

    Added achievementcard

---
+++ SpaceGame/Assets/Scripts/AchievementChecker.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AchievementChecker : MonoBehaviour
+{
+    public Text AchievementText;
+    public string AchievementName;
+    public string HowToGet;
+    public string IfGotText;
+    public bool AlsoCreateObject;
+    public GameObject CreateObject;
+    private int AchievementState;
+
+    void Start()
+    {
+        AchievementState = PlayerPrefs.GetInt(AchievementName.Replace(" ", string.Empty), 0);
+
+        if (AchievementState == 1)
+        {
+            AchievementText.text = AchievementName + ": \n" +  IfGotText;
+            if (AlsoCreateObject)
+            {
+                Instantiate(CreateObject);
+            }
+        }
+        else
+        {
+            AchievementText.text = AchievementName + ": \n" + HowToGet;
+        }
+    }
+}
