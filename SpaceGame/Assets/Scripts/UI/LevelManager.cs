f54bc6e6995444587cf8bb813413521f89b0a669
Sjors Kaagman <sjorspnk@gmail.com>
2018-06-20 10:53:03 +0200

    oof

--- SpaceGame/Assets/Scripts/UI/LevelManager.cs
+++ SpaceGame/Assets/Scripts/UI/LevelManager.cs
@@ -1,29 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour {
 
-	public void LoadLevel (string name){
-        PlayerPrefs.SetString("Level", name);
-        SceneManager.LoadScene("Loading");
+    public void LoadLevel (string name)
+    {
+       PlayerPrefs.SetString("Level", name);
+       SceneManager.LoadScene("Loading");
 	}
 
     public void InstantLoad(string name)
     {
-        SceneManager.LoadScene(name);
+       SceneManager.LoadScene(name);    
     }
 
     public void RestartLevel()
     {
-        string LevelReset = PlayerPrefs.GetString("StageRestart", "Menu");//Slaat stage op. anders menu
-        PlayerPrefs.SetString("Level", LevelReset);//Geeft volgende level mee
-        SceneManager.LoadScene("Loading");//Laad loading level
+       string LevelReset = PlayerPrefs.GetString("StageRestart", "Menu");//Slaat stage op. anders menu
+       PlayerPrefs.SetString("Level", LevelReset);//Geeft volgende level mee
+       SceneManager.LoadScene("Loading");//Laad loading level    
     }
 
-	public void QuitRequest(){
-        Debug.Log("requist");
-		Application.Quit();
+	public void QuitRequest()
+    {
+       Debug.Log("requist");
+       Application.Quit();
 	}
 }
---
+++ SpaceGame/Assets/Scripts/UI/LevelManagerTest.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelManagerTest : MonoBehaviour {
+
+    bool loadstate = false;
+    bool go = false;
+    public Image LoadImage;
+    public float TransitionSpeed = 1.0f;
+
+    public void LoadLevel (string name){
+        go = true;
+        if (loadstate)
+        {
+            PlayerPrefs.SetString("Level", name);
+            SceneManager.LoadScene("Loading");
+        }
+	}
+
+    public void InstantLoad(string name)
+    {
+        go = true;
+        if (loadstate)
+        {
+            SceneManager.LoadScene(name);
+        }
+    }
+
+    public void RestartLevel()
+    {
+        go = true;
+        if (loadstate)
+        {
+            string LevelReset = PlayerPrefs.GetString("StageRestart", "Menu");//Slaat stage op. anders menu
+            PlayerPrefs.SetString("Level", LevelReset);//Geeft volgende level mee
+            SceneManager.LoadScene("Loading");//Laad loading level
+        }
+    }
+
+	public void QuitRequest(){
+        go = true;
+        if (loadstate)
+        {
+            Debug.Log("requist");
+            Application.Quit();
+        }
+	}
+
+    void Update()
+    {
+        if (go)
+        {
+            LoadImage.fillAmount -= 1.0f / TransitionSpeed * Time.deltaTime;
+            if (LoadImage.fillAmount == 0)
+            {
+                Destroy(LoadImage);
+                loadstate = true;
+            }
+        }
+    }
+}
