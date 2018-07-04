a8b850bfd68c9adfedeade498b39a40a6d9c86d5
Sjooooors <sjorspnk@gmail.com>
2018-07-04 21:43:25 +0200

    name for speedruns

---
+++ SpaceGame/Assets/FONTS/Username.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Username : MonoBehaviour {
+    public InputField TextBox;
+    private void Start()
+    {
+        TextBox.text ="Hi! " + PlayerPrefs.GetString("Player","Player");
+    }
+    void Update () {
+        if (Input.GetKeyDown("return"))
+        {
+            PlayerPrefs.SetString("Player", TextBox.text);
+            TextBox.text = "Hi! " + PlayerPrefs.GetString("Player", "Player");
+        }
+    }
+}
--- SpaceGame/Assets/Scripts/Player/EasterEggs.cs
+++ SpaceGame/Assets/Scripts/Player/EasterEggs.cs
@@ -8,7 +8,7 @@ public class EasterEggs : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         //Random message of the day
-        int Randomizer = Random.Range(0,6);
+        int Randomizer = Random.Range(0,12);
         if (Randomizer == 0)
         {
             TextBox.text = "Play brilliant bob instead";
@@ -36,6 +36,30 @@ public class EasterEggs : MonoBehaviour {
         if (Randomizer == 6)
         {
             TextBox.text = "OOF";
+        }
+        if (Randomizer == 7)
+        {
+            TextBox.text = "Erwin is a terrible gamer";
+        }
+        if (Randomizer == 8)
+        {
+            TextBox.text = "Don't stop believing";
+        }
+        if (Randomizer == 9)
+        {
+            TextBox.text = "Potatoes are people too!";
+        }
+        if (Randomizer == 10)
+        {
+            TextBox.text = "No u";
+        }
+        if (Randomizer == 11)
+        {
+            TextBox.text = "Mamma mia!";
+        }
+        if (Randomizer == 12)
+        {
+            TextBox.text = "Spaaaaaaaaaaaaaceeeeeeee!";
         }
         if (System.DateTime.Now.Day.ToString() == "6" && System.DateTime.Now.Month.ToString() == "1")
         {
