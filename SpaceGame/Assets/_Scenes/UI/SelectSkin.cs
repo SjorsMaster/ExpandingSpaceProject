b180bbe36201e735bbe53f6d8ed6fd871e48c12a
ShaileshSSC <shaileshchotoe2001@gmail.com>
2018-07-04 20:14:32 +0200

    skins

--- SpaceGame/Assets/_Scenes/UI/SelectSkin.cs
+++ SpaceGame/Assets/_Scenes/UI/SelectSkin.cs
@@ -3,6 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SelectSkin : MonoBehaviour {
+
+    private Animator anim;
+    public GameObject player;
+
+    private void Start()
+    {
+        anim = player.GetComponent<Animator>();
+    }
+
     public void onClick(int SkinNumber)
     {
         PlayerPrefs.SetInt("Skin", SkinNumber);
@@ -12,15 +21,16 @@ public class SelectSkin : MonoBehaviour {
     {
         if (PlayerPrefs.GetInt("Skin") == 0)
         {
-            //Animator
+            anim.SetInteger("Skin", 0);
+            Debug.Log("Dit doet niks?");
         }
         if (PlayerPrefs.GetInt("Skin") == 1)
         {
-            //Animator
+            anim.SetInteger("Skin", 1);
         }
         if (PlayerPrefs.GetInt("Skin") == 2)
         {
-            //Animator
+            anim.SetInteger("Skin", 2);
         }
         if (PlayerPrefs.GetInt("Skin") == 3)
         {
